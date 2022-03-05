5aedd7b116f21c3be58815b0411a43a86f985bd9
Zechariah Dominguez <zechariahd@gmail.com>
2022-03-05 15:01:50 -0500

    Added two more components for break duration and focus duration

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -48,6 +48,84 @@ function nextSession(focusDuration, breakDuration) {
   };
 }
 
+//Defining 3 components
+
+function FocusDuration({
+  focusDuration,
+  focusDecrease,
+  focusIncrease,
+  disableFocusAndBreak,
+}) {
+  return (
+    <div className="input-group input-group-lg mb-2">
+      <span className="input-group-text" data-testid="duration-focus">
+        {/* TODO: Update this text to display the current focus session duration */}
+        Focus Duration: {minutesToDuration(focusDuration)}
+      </span>
+      <div className="input-group-append">
+        {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid="decrease-focus"
+          onClick={focusDecrease}
+          disabled={disableFocusAndBreak}
+        >
+          <span className="oi oi-minus" />
+        </button>
+        {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid="increase-focus"
+          onClick={focusIncrease}
+          disabled={disableFocusAndBreak}
+        >
+          <span className="oi oi-plus" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function BreakDuration({
+  breakDuration,
+  breakIncrease,
+  breakDecrease,
+  disableFocusAndBreak,
+}) {
+  return (
+    <div className="input-group input-group-lg mb-2">
+      <span className="input-group-text" data-testid="duration-break">
+        {/* TODO: Update this text to display the current break session duration */}
+        Break Duration: {minutesToDuration(breakDuration)}
+      </span>
+      <div className="input-group-append">
+        {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid="decrease-break"
+          onClick={breakDecrease}
+          disabled={disableFocusAndBreak}
+        >
+          <span className="oi oi-minus" />
+        </button>
+        {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid="increase-break"
+          onClick={breakIncrease}
+          disabled={disableFocusAndBreak}
+        >
+          <span className="oi oi-plus" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
 function ProgressBar({ session, focusDuration, breakDuration }) {
   const timeSeconds =
     (session?.label === "Focusing" ? focusDuration : breakDuration) * 60;
@@ -144,65 +222,21 @@ function Pomodoro() {
     <div className="pomodoro">
       <div className="row">
         <div className="col">
-          <div className="input-group input-group-lg mb-2">
-            <span className="input-group-text" data-testid="duration-focus">
-              {/* TODO: Update this text to display the current focus session duration */}
-              Focus Duration: {minutesToDuration(focusDuration)}
-            </span>
-            <div className="input-group-append">
-              {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="decrease-focus"
-                onClick={focusDecrease}
-                disabled={disableFocusAndBreak}
-              >
-                <span className="oi oi-minus" />
-              </button>
-              {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="increase-focus"
-                onClick={focusIncrease}
-                disabled={disableFocusAndBreak}
-              >
-                <span className="oi oi-plus" />
-              </button>
-            </div>
-          </div>
+          <FocusDuration
+            focusDuration={focusDuration}
+            focusIncrease={focusIncrease}
+            focusDecrease={focusDecrease}
+            disableFocusAndBreak={disableFocusAndBreak}
+          />
         </div>
         <div className="col">
           <div className="float-right">
-            <div className="input-group input-group-lg mb-2">
-              <span className="input-group-text" data-testid="duration-break">
-                {/* TODO: Update this text to display the current break session duration */}
-                Break Duration: {minutesToDuration(breakDuration)}
-              </span>
-              <div className="input-group-append">
-                {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-testid="decrease-break"
-                  onClick={breakDecrease}
-                  disabled={disableFocusAndBreak}
-                >
-                  <span className="oi oi-minus" />
-                </button>
-                {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-testid="increase-break"
-                  onClick={breakIncrease}
-                  disabled={disableFocusAndBreak}
-                >
-                  <span className="oi oi-plus" />
-                </button>
-              </div>
-            </div>
+            <BreakDuration
+              breakDuration={breakDuration}
+              breakIncrease={breakIncrease}
+              breakDecrease={breakDecrease}
+              disableFocusAndBreak={disableFocusAndBreak}
+            />
           </div>
         </div>
       </div>
